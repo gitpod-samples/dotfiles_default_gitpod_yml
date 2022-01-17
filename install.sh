1bcae1b34adb466b115c1af183377703b2f04fbe
AXON <axonasif@gmail.com>
2022-01-17 23:38:32 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+echo $TEST_VAR
 _source_dir="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}";
 if test ! -e "$GITPOD_REPO_ROOT/.gitpod.yml"; then {
     cp "$_source_dir/.gitpod.default.yml" "$GITPOD_REPO_ROOT/.gitpod.yml";
