4252af314ecd73f5e86dcd6a838784b7903340f5
AXON <axonasif@gmail.com>
2022-01-24 09:45:36 +0000

    Update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -eu;
 _source_dir="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}";
 for _file in '.default.gitpod.yml' '.default.gitpod.Dockerfile'; do {
     _file_final="${_file#.default}";
-    if test -e "$HOME/$_file_final" && test ! -e "$GITPOD_REPO_ROOT/$_file_final"; then {
+    if test -e "$_source_dir/$_file_final" && test ! -e "$GITPOD_REPO_ROOT/$_file_final"; then {
         cp "$_source_dir/$_file" "$GITPOD_REPO_ROOT/$_file_final";
         # if ! grep "/$_file_final" "$GITPOD_REPO_ROOT/.gitignore" 1>/dev/null; then {
         #     printf "/$_file_final\n" >> "$GITPOD_REPO_ROOT/.gitignore";
