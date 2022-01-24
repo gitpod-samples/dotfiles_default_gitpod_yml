5846a1efb7c13427a4f6ea8b00585d8e1da1000b
AXON <axonasif@gmail.com>
2022-01-24 15:24:49 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
-echo $TEST_VAR
+
+set -eu;
+
 _source_dir="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}";
-if test ! -e "$GITPOD_REPO_ROOT/.gitpod.yml"; then {
-    cp "$_source_dir/.gitpod.default.yml" "$GITPOD_REPO_ROOT/.gitpod.yml";
+for _file in '.default.gitpod.yml' '.default.gitpod.Dockerfile'; do {
+    _file_final="${_file#.default}";
+    if test ! -e "$GITPOD_REPO_ROOT/$_file_final"; then {
+        cp "$_source_dir/$_file" "$GITPOD_REPO_ROOT/$_file_final";
+        if ! grep "/$_file_final" "$GITPOD_REPO_ROOT/.gitignore" 1>/dev/null; then {
+            printf "/$_file_final\n" >> "$GITPOD_REPO_ROOT/.gitignore";
+            _gitignore_modified=true;
+        } fi
+    } fi
+}
+
+if test "${_gitignore_modified:-}" == true; then {
+    git add "$GITPOD_REPO_ROOT/.gitignore";
 } fi
